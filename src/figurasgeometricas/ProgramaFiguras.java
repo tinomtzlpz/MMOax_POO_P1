3aea9842d43182a768a8f122cd3e07b8212cc0f0
tinom <tinom@MOB-TPW541>
2018-09-16 09:24:09 -0500

    Se agregaron en el programa principal los casos para las figuras pentágono y trapezoide regular. Estos casos obtienen los datos del usuario y crean los correspondientes objetos para que realicen los cálculos de perímetro y área.

--- src/figurasgeometricas/ProgramaFiguras.java
+++ src/figurasgeometricas/ProgramaFiguras.java
@@ -22,13 +22,37 @@ public class ProgramaFiguras {
         System.out.println("1.- Círculo.");  
         System.out.println("2.- Rectángulo.");  
         System.out.println("3.- Pentágono.");  
-        System.out.println("4.- Trapezoide");  
+        System.out.println("4.- Trapezoide regular");  
         System.out.println("5.- Octágono");
         System.out.println("Selecciona una opción..."); 
         
         int opcion = 0;
         opcion = sc.nextInt();
-        System.out.println("Escogiste la opcion: ");
-        System.out.println(String.valueOf(opcion));
+        switch(opcion){
+            case 3: System.out.println("Por favor, ingresa el valor del apotema: ");
+                    float apotema = sc.nextFloat();
+                    System.out.println("Por favor, ingresa el valor del lado: ");
+                    float lado = sc.nextFloat();
+                    Pentagono varPentagono = new Pentagono(apotema, lado);
+                    System.out.println("El perímetro del pentágono es:");
+                    System.out.println(varPentagono.calculaPerimetro());
+                    System.out.println("El área del pentágono es:");
+                    System.out.println(varPentagono.calculaArea());
+                    break;
+            case 4: System.out.println("Por favor, ingresa el valor de la base mayor: ");
+                    float baseMayor = sc.nextFloat();
+                    System.out.println("Por favor, ingresa el valor de la base menor: ");
+                    float baseMenor = sc.nextFloat();
+                    System.out.println("Por favor, ingresa el valor de la altura: ");
+                    float altura = sc.nextFloat();
+                    Trapezoide varTrapezoide = new Trapezoide(baseMayor, baseMenor, altura, altura);
+                    System.out.println("El perímetro del trapezoide regular es:");
+                    System.out.println(varTrapezoide.calculaPerimetro());
+                    System.out.println("El área del trapezoide regular es:");
+                    System.out.println(varTrapezoide.calculaArea());
+                    
+        }
+        //System.out.println("Escogiste la opcion: ");
+        //System.out.println(String.valueOf(opcion));
     }    
 }
