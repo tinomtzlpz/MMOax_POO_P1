f883f54c177919911df163d424b96bb71a4c16a5
luisreyes26 <43220993+luisreyes26@users.noreply.github.com>
2018-09-17 19:26:53 -0500

    Add files via upload
    Agregue la clase cuadrado, que hereda de la clase FigGeometrica y es la primera version.

---
+++ src/figurasgeometricas/Cuadrado.java
@@ -0,0 +1,46 @@
+
+
+/**
+ *
+ * @author Sony
+ */
+
+
+
+public class Cuadrado extends FigGeometrica{
+      
+    
+   private int base;
+   private int altura;
+
+    public Cuadrado(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+    public float calculaArea(){
+        return base*altura;
+    }
+    
+    public float calculaPerimetro(){
+        return  (base*2)+(altura*2);
+    }
+    
+    
+}
